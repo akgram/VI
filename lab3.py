e5ba6cbeefb4ab56f9bd830a4a76f3a2d0bf8543
Aleksandar <akrstic@elfak.rs>
2024-11-14 14:02:47 +0100

    lab3

--- lab3.py
+++ lab3.py
@@ -1,64 +1,36 @@
-#17. Napisati funkciju koja na osnovu zadatog težinskog neusmerenog grafa i zadatog (ciljnog) čvora G
-#formira neusmereni težinski graf sa heuristikom. Heuristika proizvoljnog čvora C se određuje kao
-#dužina puta od čvora C do čvora G. 
+#9. Napisati funkciju koja određuje broj čvorova do kojih može da se dođe od zadatog čvora, tako da
+#je dužina puta do čvora jednaka zadatoj vrednosti. Obići samo neophodne čvorove. 
 
+from collections import deque
 
-
-#from multiprocessing import process
-import queue
-
-
-def hill_climbing_search(graph, start, end):
-    if start is end:
-        path = []
-        path.append(start)
-        return path
-    stack_nodes = queue.LifoQueue(len(graph)) #LifoQueue
+def dist(graph, start, target_dist):
+    queue = deque([(start, 0)])
     visited = set()
-    prev_nodes = dict()
-    prev_nodes[start] = None
+    count = 0
     visited.add(start)
-    stack_nodes.put(start)
-    found_dest = False
+    #queue.put(start)
+    
+    while queue:
+        node, distance = queue.popleft()
+        
+        if distance == target_dist:
+            count += 1
+            continue
+        
+        for neighbor in graph[node]:
+            new_distance = distance + 1
+            if neighbor not in visited and new_distance <= target_dist:
+                visited.add(neighbor)
+                queue.append((neighbor, new_distance))
+    
+    return count
 
-    while (not found_dest) and (not stack_nodes.empty()):
-        node = stack_nodes.get()
-        #process(node)
-        destinations = []
-        for dest in graph[node][1]:
-            element = (graph[dest][0], dest)
-            destinations.append(element)
-        for dest_heur in sorted(destinations, reverse=True):
-            if dest_heur[1] not in visited:
-                prev_nodes[dest_heur[1]] = node
-                if dest_heur[1] is end:
-                    found_dest = True
-                    break
-                visited.add(dest_heur[1])
-                stack_nodes.put(dest_heur[1])
-
-    path = []
-    if found_dest:
-        path.append(end)
-        prev = prev_nodes[end]
-        while prev is not None:
-            path.append(prev)
-            prev = prev_nodes[prev]
-        path.reverse()
-    return path
-
-graph_simple = {
-'A' : (4, ['B','C']),
-'B' : (3, ['D', 'E']),
-'C' : (2, ['F', 'G']),
-'D' : (9, ['H']),
-'E' : (2, ['G', 'I']),
-'F' : (1, ['J']),
-'G' : (1, ['J']),
-'H' : (9, []),
-'I' : (1, ['J']),
-'J' : (0, [])
+graph = {
+    1: {2: 2, 3: 1},
+    2: {1: 3, 4: 2},
+    3: {1: 1, 4: 4},
+    4: {2: 5, 3: 3, 5: 1},
+    5: {4: 1}
 }
-
-path = hill_climbing_search(graph_simple, 'A', 'J')
-print(path)
+result = dist(graph, 4, 2)
+print(result)
